5ee017d73ddabd62d829687244c652baa11fb834
abetusk <abetusk@gmail.com>
2021-04-16 17:31:33 -0400

    more verbose license header

--- src/tarot/divine.js
+++ src/tarot/divine.js
@@ -1,4 +1,8 @@
-// cc0
+// License: CC0
+//
+// To the extent possible under law, the person who associated CC0 with
+// this file has waived all copyright and related or neighboring rights
+// to this file.
 //
 
 
