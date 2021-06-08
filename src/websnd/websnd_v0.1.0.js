cc5a23f47daa8103b327a99193a72058545cb17a
abetusk <abetusk@gmail.com>
2021-06-08 14:12:53 -0400

    experiments

--- src/websnd/websnd_v0.1.0.js
+++ src/websnd/websnd_v0.1.0.js
@@ -44,6 +44,264 @@ const NOTES = {
 
 };
 
+// ***************************************
+// ***************************************
+// ***
+// *** https://github.com/velipso/adsrnode
+// ***
+// ***************************************
+// ***************************************
+
+// (c) Copyright 2018, Sean Connelly (@voidqk), http://sean.cm
+// MIT License
+// Project Home: https://github.com/voidqk/adsrnode
+
+(function(){
+
+var DEBUG = false;
+
+function ADSRNode(ctx, opts){
+	// `ctx` is the AudioContext
+	// `opts` is an object in the format:
+	// {
+	//   base:         <number>, // output     optional    default: 0
+	//   attack:       <number>, // seconds    optional    default: 0
+	//   attackCurve:  <number>, // bend       optional    default: 0
+	//   peak:         <number>, // output     optional    default: 1
+	//   hold:         <number>, // seconds    optional    default: 0
+	//   decay:        <number>, // seconds    optional    default: 0
+	//   decayCurve:   <number>, // bend       optional    default: 0
+	//   sustain:      <number>, // output     required
+	//   release:      <number>, // seconds    optional    default: 0
+	//   releaseCurve: <number>  // bend       optional    default: 0
+	// }
+
+	function getNum(opts, key, def){
+		if (typeof def === 'number' && typeof opts[key] === 'undefined')
+			return def;
+		if (typeof opts[key] === 'number')
+			return opts[key];
+		throw new Error('[ADSRNode] Expecting "' + key + '" to be a number');
+	}
+
+	var attack = 0, decay = 0, sustain, sustain_adj, release = 0;
+	var base = 0, acurve = 0, peak = 1, hold = 0, dcurve = 0, rcurve = 0;
+
+	function update(opts){
+		base    = getNum(opts, 'base'        , base   );
+		attack  = getNum(opts, 'attack'      , attack );
+		acurve  = getNum(opts, 'attackCurve' , acurve );
+		peak    = getNum(opts, 'peak'        , peak   );
+		hold    = getNum(opts, 'hold'        , hold   );
+		decay   = getNum(opts, 'decay'       , decay  );
+		dcurve  = getNum(opts, 'decayCurve'  , dcurve );
+		sustain = getNum(opts, 'sustain'     , sustain);
+		release = getNum(opts, 'release'     , release);
+		rcurve  = getNum(opts, 'releaseCurve', rcurve );
+		sustain_adj = adjustCurve(dcurve, peak, sustain);
+	}
+
+	// extract options
+	update(opts);
+
+	// create the node and inject the new methods
+	var node = ctx.createConstantSource();
+	node.offset.value = base;
+
+	// unfortunately, I can't seem to figure out how to use cancelAndHoldAtTime, so I have to have
+	// code that calculates the ADSR curve in order to figure out the value at a given time, if an
+	// interruption occurs
+	//
+	// the curve functions (linearRampToValueAtTime and setTargetAtTime) require an *event*
+	// preceding the curve in order to calculate the correct start value... inserting the event
+	// *should* work with cancelAndHoldAtTime, but it doesn't (or I misunderstand the API).
+	//
+	// therefore, for the curves to start at the correct location, I need to be able to calculate
+	// the entire ADSR curve myself, so that I can correctly interrupt the curve at any moment.
+	//
+	// these values track the state of the trigger/release moments, in order to calculate the final
+	// curve
+	var lastTrigger = false;
+	var lastRelease = false;
+
+	// small epsilon value to check for divide by zero
+	var eps = 0.00001;
+
+	function curveValue(type, startValue, endValue, curTime, maxTime){
+		if (type === 0)
+			return startValue + (endValue - startValue) * Math.min(curTime / maxTime, 1);
+		// otherwise, exponential
+		return endValue + (startValue - endValue) * Math.exp(-curTime * type / maxTime);
+	}
+
+	function adjustCurve(type, startValue, endValue){
+		// the exponential curve will never hit its target... but we can calculate an adjusted
+		// target so that it will miss the adjusted value, but end up hitting the actual target
+		if (type === 0)
+			return endValue; // linear hits its target, so no worries
+		var endExp = Math.exp(-type);
+		return (endValue - startValue * endExp) / (1 - endExp);
+	}
+
+	function triggeredValue(time){
+		// calculates the actual value of the envelope at a given time, where `time` is the number
+		// of seconds after a trigger (but before a release)
+		var atktime = lastTrigger.atktime;
+		if (time < atktime){
+			return curveValue(acurve, lastTrigger.v,
+				adjustCurve(acurve, lastTrigger.v, peak), time, atktime);
+		}
+		if (time < atktime + hold)
+			return peak;
+		if (time < atktime + hold + decay)
+			return curveValue(dcurve, peak, sustain_adj, time - atktime - hold, decay);
+		return sustain;
+	}
+
+	function releasedValue(time){
+		// calculates the actual value of the envelope at a given time, where `time` is the number
+		// of seconds after a release
+		if (time < 0)
+			return sustain;
+		if (time > lastRelease.reltime)
+			return base;
+		return curveValue(rcurve, lastRelease.v,
+			adjustCurve(rcurve, lastRelease.v, base), time, lastRelease.reltime);
+	}
+
+	function curveTo(param, type, value, time, duration){
+		if (type === 0 || duration <= 0)
+			param.linearRampToValueAtTime(value, time + duration);
+		else // exponential
+			param.setTargetAtTime(value, time, duration / type);
+	}
+
+	node.trigger = function(when){
+		if (typeof when === 'undefined')
+			when = this.context.currentTime;
+
+		if (lastTrigger !== false){
+			if (when < lastTrigger.when)
+				throw new Error('[ADSRNode] Cannot trigger before future trigger');
+			this.release(when);
+		}
+		var v = base;
+		var interruptedLine = false;
+		if (lastRelease !== false){
+			var now = when - lastRelease.when;
+			v = releasedValue(now);
+			// check if a linear release has been interrupted by this attack
+			interruptedLine = rcurve === 0 && now >= 0 && now <= lastRelease.reltime;
+			lastRelease = false;
+		}
+		var atktime = attack;
+		if (Math.abs(base - peak) > eps)
+			atktime = attack * (v - peak) / (base - peak);
+		lastTrigger = { when: when, v: v, atktime: atktime };
+
+		this.offset.cancelScheduledValues(when);
+
+		if (DEBUG){
+			// simulate curve using triggeredValue (debug purposes)
+			for (var i = 0; i < 10; i += 0.01)
+				this.offset.setValueAtTime(triggeredValue(i), when + i);
+			return this;
+		}
+
+		if (interruptedLine)
+			this.offset.linearRampToValueAtTime(v, when);
+		else
+			this.offset.setTargetAtTime(v, when, 0.001);
+		curveTo(this.offset, acurve, adjustCurve(acurve, v, peak), when, atktime);
+		this.offset.setTargetAtTime(peak, when + atktime, 0.001);
+		if (hold > 0)
+			this.offset.setTargetAtTime(peak, when + atktime + hold, 0.001);
+		curveTo(this.offset, dcurve, sustain_adj, when + atktime + hold, decay);
+		this.offset.setTargetAtTime(sustain, when + atktime + hold + decay, 0.001);
+		return this;
+	};
+
+	node.release = function(when){
+		if (typeof when === 'undefined')
+			when = this.context.currentTime;
+
+		if (lastTrigger === false)
+			throw new Error('[ADSRNode] Cannot release without a trigger');
+		if (when < lastTrigger.when)
+			throw new Error('[ADSRNode] Cannot release before the last trigger');
+		var tnow = when - lastTrigger.when;
+		var v = triggeredValue(tnow);
+		var reltime = release;
+		if (Math.abs(sustain - base) > eps)
+			reltime = release * (v - base) / (sustain - base);
+		lastRelease = { when: when, v: v, reltime: reltime };
+		var atktime = lastTrigger.atktime;
+		// check if a linear attack or a linear decay has been interrupted by this release
+		var interruptedLine =
+			(acurve === 0 && tnow >= 0 && tnow <= atktime) ||
+			(dcurve === 0 && tnow >= atktime + hold && tnow <= atktime + hold + decay);
+		lastTrigger = false;
+
+		this.offset.cancelScheduledValues(when);
+		node.baseTime = when + reltime;
+
+		if (DEBUG){
+			// simulate curve using releasedValue (debug purposes)
+			for (var i = 0; true; i += 0.01){
+				this.offset.setValueAtTime(releasedValue(i), when + i);
+				if (i >= reltime)
+					break;
+			}
+			return this;
+		}
+
+		if (interruptedLine)
+			this.offset.linearRampToValueAtTime(v, when);
+		else
+			this.offset.setTargetAtTime(v, when, 0.001);
+		curveTo(this.offset, rcurve, adjustCurve(rcurve, v, base), when, reltime);
+		this.offset.setTargetAtTime(base, when + reltime, 0.001);
+		return this;
+	};
+
+	node.reset = function(){
+		lastTrigger = false;
+		lastRelease = false;
+		var now = this.context.currentTime;
+		this.offset.cancelScheduledValues(now);
+		this.offset.setTargetAtTime(base, now, 0.001);
+		node.baseTime = now;
+		return this;
+	};
+
+	node.update = function(opts){
+		update(opts);
+		return this.reset();
+	};
+
+	node.baseTime = 0;
+
+	return node;
+}
+
+// export appropriately
+if (typeof window === 'undefined')
+	module.exports = ADSRNode;
+else
+	window.ADSRNode = ADSRNode;
+
+})();
+
+// ***************************************
+// ***************************************
+// ***
+// *** https://github.com/velipso/adsrnode
+// ***
+// ***************************************
+// ***************************************
+
+
+
 function _defineProperty(obj, key, value) {
   if (key in obj) {
     Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });
@@ -185,6 +443,104 @@ class Synth {
       (freq, t = 0) => {
         Synth.AC.resume();
 
+        //this.killOscillators(t);
+
+        const ct = Synth.AC.currentTime;
+
+        //--
+
+        this.nodes.adsr_envelope.update( {
+          //"attack":   this.params.adsrAttack,
+          "attack":   2.0,
+          //"decay":    this.params.adsrDecay,
+          "decay":    0.2,
+          //"sustain":  this.params.adsrSustain,
+          "sustain":  0.5,
+          //"release":  this.params.adsrRelease
+          "release":  0.5
+        });
+
+        /*
+        var adsrTarget = this.nodes.adsr.gain;
+        this.nodes.adsr.gain.setValueAtTime(1, ct);
+        var atkDuration = this.params.adsrAttack * Synth.MAX_ADSR_STAGE_DURATION;
+        adsrTarget.setValueAtTime(this.getADSRValue(0), ct);
+        adsrTarget.linearRampToValueAtTime(this.getADSRValue(1), ct + atkDuration);
+
+        var decayDuration = this.params.adsrDecay * Synth.MAX_ADSR_STAGE_DURATION;
+        adsrTarget.setTargetAtTime(this.getADSRValue(this.params.adsrSustain), ct + atkDuration, decayDuration);
+        */
+
+        //--
+
+        this.nodes.filter_envelope.update( {
+          "attack":   this.params.adsrFilterAttack,
+          "decay":    this.params.adsrFilterDecay,
+          "sustain":  this.params.adsrFilterSustain,
+          "release":  this.params.adsrFilterRelease
+        });
+
+        /*
+        var adsrTarget = this.nodes.filter.frequency;
+        this.nodes.filter.frequency.setValueAtTime(this.calcFreqValue(this.params.filterFreq), ct);
+        var atkDuration = this.params.adsrFilterAttack * Synth.MAX_ADSR_STAGE_DURATION;
+        adsrTarget.setValueAtTime(this.getADSRFilterValue(0), ct);
+        adsrTarget.linearRampToValueAtTime(this.getADSRFilterValue(1), ct + atkDuration);
+
+        var decayDuration = this.params.adsrFilterDecay * Synth.MAX_ADSR_STAGE_DURATION;
+        adsrTarget.setTargetAtTime(this.getADSRFilterValue(this.params.adsrFilterSustain), ct + atkDuration, decayDuration);
+        */
+
+        //--
+
+        var width = this.getUnisonWidth(this.params.unisonWidth);
+
+        this.oscillators[0] = this.createOscillator(freq, this.params.waveform);
+        this.oscillators[0].start(t);
+
+        //this.oscillators[1] = this.createOscillator(freq, this.params.waveform, -width);
+        //this.oscillators[2] = this.createOscillator(freq, this.params.waveform, width);
+        //this.oscillators.forEach(osc => osc.start(t));
+
+        this.nodes.adsr_envelope.trigger();
+        this.nodes.filter_envelope.trigger();
+      }
+    );
+
+    _defineProperty(this, "noteOff",
+
+      (t = 0) => {
+        const ct = Synth.AC.currentTime;
+
+        this.nodes.adsr_envelope.release(ct);
+        this.nodes.filter_envelope.release(ct);
+
+        /*
+
+        var relDuration = (this.params.adsrAttack + this.params.adsrDecay + this.params.adsrRelease) * Synth.MAX_ADSR_STAGE_DURATION;
+        var adsrTarget = this.nodes.adsr.gain;
+        adsrTarget.setValueAtTime(adsrTarget.value, ct);
+        adsrTarget.linearRampToValueAtTime(this.getADSRValue(0), ct + relDuration);
+
+        this.killOscillators(ct + 100*relDuration);
+        console.log(">>", ct, relDuration);
+        console.log(">>>", ct, ct + 4*relDuration, this.params.adsrAttack, this.params.adsrDecay, this.params.adsrSustain, this.params.adsrRelease);
+
+
+        relDuration = (this.params.adsrFilterAttack + this.params.adsrFilterDecay + this.params.adsrFilterRelease) * Synth.MAX_ADSR_STAGE_DURATION;
+        adsrTarget = this.nodes.filter.frequency;
+        adsrTarget.setValueAtTime(adsrTarget.value, ct);
+        adsrTarget.linearRampToValueAtTime(this.getADSRFilterValue(0), ct + relDuration);
+        */
+      }
+    );
+
+
+    _defineProperty(this, "noteOn_orig",
+
+      (freq, t = 0) => {
+        Synth.AC.resume();
+
         this.killOscillators(t);
 
         const ct = Synth.AC.currentTime;
@@ -224,7 +580,7 @@ class Synth {
     );
 
 
-    _defineProperty(this, "noteOff",
+    _defineProperty(this, "noteOff_orig",
 
       (t = 0) => {
         const ct = Synth.AC.currentTime;
@@ -341,15 +697,38 @@ class Synth {
     this.analyserBufferLength = this.nodes.analyser.frequencyBinCount;
     this.analyserData = new Uint8Array(this.analyserBufferLength);
     this.nodes.adsr.connect(this.nodes.filter);
-    this.nodes.filter.connect(this.nodes.delay);
+
+    //this.nodes.filter.connect(this.nodes.delay);
+
     this.nodes.delay.connect(this.nodes.feedback);
     this.nodes.feedback.connect(this.nodes.delay);
     this.nodes.filter.connect(this.nodes.volume);
-    this.nodes.feedback.connect(this.nodes.volume);
+    //this.nodes.feedback.connect(this.nodes.volume);
+
     this.nodes.volume.connect(this.nodes.analyser);
     this.nodes.analyser.connect(Synth.AC.destination);
     this.sequence = ['C-3', 'D#3', 'G-3', 'C-3', 'D-3', 'D#3', 'C-3', 'D-3', 'D#3', 'C-3', 'D#3', 'G#3', 'C-3', 'G-3', 'C-3', 'G-3'];
     this.isPlaying = false;
+
+    this.nodes.adsr_envelope = ADSRNode(Synth.AC, {
+      "attack":   this.params.adsrAttack,
+      "decay":    this.params.adsrDecay,
+      "sustain":  this.params.adsrSustain,
+      "release":  this.params.adsrRelease
+    });
+    this.nodes.adsr_envelope.connect(this.nodes.adsr.gain);
+    this.nodes.adsr_envelope.start();
+
+    this.nodes.filter_envelope = ADSRNode(Synth.AC, {
+      "attack":   this.params.adsrFilterAttack,
+      "decay":    this.params.adsrFilterDecay,
+      "sustain":  this.params.adsrFilterSustain,
+      "release":  this.params.adsrFilterRelease
+    });
+    //this.nodes.filter_envelope.connect(this.nodes.filter.frequency);
+    this.nodes.filter_envelope.start();
+
+
   }
 }
 
