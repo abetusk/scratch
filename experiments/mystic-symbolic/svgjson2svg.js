bbd24dc40992d383ed429db5349c25f597fbf92f
abetusk <abetusk@gmail.com>
2021-06-20 05:53:47 -0400

    ...

--- experiments/mystic-symbolic/svgjson2svg.js
+++ experiments/mystic-symbolic/svgjson2svg.js
@@ -1,5 +1,92 @@
 var fs = require("fs");
 
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+// https://stackoverflow.com/users/96100/tim-down
+//
+function _tohex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function _rgb2hex(r, g, b) {
+  return "#" + _tohex(r) + _tohex(g) + _tohex(b);
+}
+
+//  https://stackoverflow.com/a/17243070
+// From user Paul S. (https://stackoverflow.com/users/1615483/paul-s)
+//
+/* accepts parameters
+ * h  Object = {h:x, s:y, v:z}
+ * OR 
+ * h, s, v
+ * 0 <= h,s,v, <=1
+*/
+function HSVtoRGB(h, s, v) {
+  var r, g, b, i, f, p, q, t;
+  if (arguments.length === 1) { s = h.s, v = h.v, h = h.h; }
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255)
+  };
+}
+
+/* accepts parameters
+ * r  Object = {r:x, g:y, b:z}
+ * OR 
+ * r, g, b
+ *
+ * 0 <= r,g,b <= 255
+*/
+function RGBtoHSV(r, g, b) {
+  if (arguments.length === 1) { g = r.g, b = r.b, r = r.r; }
+  var max = Math.max(r, g, b), min = Math.min(r, g, b),
+    d = max - min,
+    h,
+    s = (max === 0 ? 0 : d / max),
+    v = max / 255;
+
+  switch (max) {
+    case min: h = 0; break;
+    case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
+    case g: h = (b - r) + d * 2; h /= 6 * d; break;
+    case b: h = (r - g) + d * 4; h /= 6 * d; break;
+  }
+
+  return { h: h, s: s, v: v };
+}
+
+function HSVtoHSL(h, s, v) {
+  if (arguments.length === 1) { s = h.s, v = h.v, h = h.h; }
+  var _h = h,
+    _s = s * v, _l = (2 - s) * v;
+  _s /= (_l <= 1) ? _l : 2 - _l;
+  _l /= 2;
+  return { h: _h, s: _s, l: _l };
+}
+
+function HSLtoHSV(h, s, l) {
+  if (arguments.length === 1) { s = h.s, l = h.l, h = h.h; }
+  var _h = h, _s, _v; l *= 2;
+  s *= (l <= 1) ? l : 2 - l;
+  _v = (l + s) / 2;
+  _s = (2 * s) / (l + s);
+  return { h: _h, s: _s, v: _v };
+}
+
 // ------------------
 // ------------------
 // ------------------
@@ -100,18 +187,17 @@ function jsonsvg2svg_def(x) {
   return lines;
 }
 
-function jsonsvg2svg_defs(defs) {
-
+function jsonsvg2svg_defs(defs, primary_color, secondary_color) {
   if (typeof defs === "undefined") { return ""; }
 
+  var lines = [];
   for (var def_idx=0; def_idx<defs.length; def_idx++) {
     var x = defs[def_idx];
     var _type = x.type;
 
-    var lines = [];
-    if (_type == "linearGradient") {
+    if ((_type == "linearGradient") || (_type == "radialGradient")) {
 
-      var _line = "<linearGradient ";
+      var _line = "<" + _type + " ";
 
       for (var _key in x) {
         if ((_key === "stops") ||
@@ -129,7 +215,18 @@ function jsonsvg2svg_defs(defs) {
           var _stop_line = "<stop ";
           for (var _key in x.stops[ii]) {
             if (_key === "color" ) {
-              _stop_line += " style=\"stop-color:" + x.stops[ii][_key] + ";stop-opacity:1.0;\"";
+              var c = x.stops[ii][_key];
+
+              if ((typeof primary_color !== "undefined") && (c == "#ffffff")) {
+                c = primary_color;
+              }
+              else if ((typeof secondary_color !== "undefined") && (c == "#000000")) {
+                c = secondary_color;
+              }
+
+              //_stop_line += " style=\"stop-color:" + x.stops[ii][_key] + ";stop-opacity:1.0;\"";
+              _stop_line += " style=\"stop-color:" + c + ";stop-opacity:1.0;\"";
+
               continue;
             }
             _stop_line += " " + _key + "=\"" + x.stops[ii][_key] + "\"";
@@ -139,7 +236,7 @@ function jsonsvg2svg_defs(defs) {
         }
       }
 
-      lines.push("</linearGradient>")
+      lines.push("</" + _type + ">\n")
     }
   }
 
@@ -188,8 +285,17 @@ function jsonsvg2svg(_x) {
   return lines;
 }
 
-function jsonsvg2svg_child(x) {
+function jsonsvg2svg_child(x, primary_color, secondary_color) {
+  custom_prop = ((typeof custom_prop === "undefined") ? {} : custom_prop);
   var lines = [];
+
+  var remap = {
+    "fillRule": "fill-rule",
+    "strokeWidth":"stroke-width",
+    "strokeLinejoin":"stroke-linejoin",
+    "strokeLinecap":"stroke-linecap",
+    "vectorEffect":"vector-effect"
+  };
 
   for (var ii=0; ii<x.length; ii++) {
     _json = x[ii];
@@ -204,14 +310,32 @@ function jsonsvg2svg_child(x) {
 
     if ("props" in _json) {
       for (var prop_key in _json.props) {
-        _line += " " + prop_key + "=\"" + _json.props[prop_key] + "\"";
+
+        var _val = _json.props[prop_key];
+        if ((prop_key == "fill") || (prop_key == "stroke")) {
+
+          if ((typeof primary_color !== "undefined") && (_val == "#ffffff")) {
+            _val = primary_color;
+          }
+          else if ((typeof secondary_color !== "undefined") && (_val == "#000000")) {
+            _val = secondary_color;
+          }
+        }
+
+        var real_prop_key = prop_key;
+        if (real_prop_key in remap) {
+          real_prop_key = remap[prop_key];
+        }
+
+        _line += " " + real_prop_key + "=\"" + _val + "\"";
+
       }
     }
 
     if ((tag !== "path") && (tag.length>0)) { _line += ">\n"; }
 
     if ("children" in _json) {
-      var _d = jsonsvg2svg_child(_json.children);
+      var _d = jsonsvg2svg_child(_json.children, primary_color, secondary_color);
       _line += _d.join("\n");
     }
 
@@ -228,7 +352,7 @@ function jsonsvg2svg_child(x) {
   return lines;
 }
 
-function _preprocess_svgjson(adata) {
+function _preprocess_svgjson(adata, primary_color, secondary_color) {
   var xdata = {};
 
   for (var idx=0; idx<adata.length; idx++) {
@@ -262,7 +386,7 @@ function _preprocess_svgjson(adata) {
     //var _svg = _l.join("\n");
 
     var _svg_inner = [];
-    _svg_inner = jsonsvg2svg_child(data.layers);
+    _svg_inner = jsonsvg2svg_child(data.layers, primary_color, secondary_color);
 
     adata[idx]["svg_header"] = ['<?xml version="1.0" encoding="utf-8"?>',
       '<!-- Generator: Moho 13.5 build 20210422 -->',
@@ -282,15 +406,6 @@ function _preprocess_svgjson(adata) {
 
   return xdata;
 }
-
-var g_data = _preprocess_svgjson(adata);
-g_data["cur_depth"] = 0;
-g_data["max_depth"] = 2;
-g_data["scale"] = 0.5;
-g_data["complexity"] = 4;
-
-g_data["svg_width"] = 720.0;
-g_data["svg_height"] = 720.0;
 
 //            crown
 //      horn          horn
@@ -341,33 +456,111 @@ g_data["svg_height"] = 720.0;
 //
 //
 //
-function _recur(ctx, base, sched) {
+function mystic_symbolic_random(ctx, base, primary_color, secondary_color) {
   if (typeof ctx === "undefined") { return ""; }
+  base = ( (typeof base === "undefined") ? ctx.data[ _irnd(ctx.data.length) ] : base ) ;
+  //var primary_color = "#ff0000";
+  //var secondary_color = "#0000ff";
+
+  var _include_background_rect = true;
 
   var scale = ctx.scale;
   var complexity = ctx.complexity;
 
   var top_level = false;
 
-  if (ctx.cur_depth >= ctx.max_depth) { return ""; }
   if (ctx.cur_depth == 0) { top_level = true; }
-  ctx.cur_depth+=1;
+  ctx.cur_depth++;
 
   var ret_str = "";
 
   if (top_level) {
     ret_str += base.svg_header;
-    ret_str += jsonsvg2svg_defs(base.defs);
 
+    ret_str += "<g transform=\"translate(360 360) scale(0.5 0.5) translate(-360 -360)\">\n";
+
+    if (_include_background_rect) {
+      _bg = "#000000";
+      ret_str += "<rect x=\"-720\" y=\"-720\" width=\"2160\" height=\"2160\" fill=\"" + _bg + "\" data-is-background=\"true\">\n</rect>\n";
+    }
+    ret_str += jsonsvg2svg_defs(base.defs, primary_color, secondary_color);
   }
 
   var base_specs = base.specs;
   var base_meta = base.meta;
   var base_bbox = base.bbox;
 
+  if (ctx.cur_depth <= ctx.max_depth) {
+
+    // attach to logic
+    //
+    var candidate_attach_list = [];
+    for (var spec_key in base_specs) {
+      if ((spec_key === "anchor") || (spec_key === "nesting")) { continue; }
+      candidate_attach_list.push(spec_key);
+    }
+    var attach_list = _choose(candidate_attach_list, complexity);
+
+    for (var attach_list_idx=0; attach_list_idx < attach_list.length; attach_list_idx++) {
+      var sub_idx = _irnd(ctx.data.length);
+      var sub_name = ctx.data[sub_idx].name;
+
+      var attach_id = attach_list[attach_list_idx];
+
+      var reuse_svg = "";
+      for (var aidx=0; aidx < base.specs[attach_id].length; aidx++) {
+
+        var sub = Object.assign({}, ctx.symbol[sub_name]);
+
+        var _invert = ( ((aidx%2)==0) ? false : true );
+        var f = (_invert ? -1.0 : 1.0);
+
+        var base_attach_point = [ base.specs[attach_id][aidx].point.x, base.specs[attach_id][aidx].point.y ];
+        var base_attach_deg = _deg( base.specs[attach_id][aidx].normal.x, base.specs[attach_id][aidx].normal.y );
+
+        var sub_anchor_point = [ sub.specs.anchor[0].point.x, sub.specs.anchor[0].point.y ];
+        var sub_anchor_deg = _deg( sub.specs.anchor[0].normal.x, f*sub.specs.anchor[0].normal.y );
+
+        var deg = base_attach_deg - sub_anchor_deg;
+        if (_invert) { deg *= -1; }
+
+        var t_str_s = "<g transform=\"";
+        t_str_s += " translate(" + base_attach_point[0].toString() + " " + base_attach_point[1].toString() + ")";
+        t_str_s += " scale(" + scale.toString() + " " + (f*scale).toString() + ")";
+        t_str_s += " rotate(" + (deg).toString() + ")";
+        t_str_s += " translate(" + (-sub_anchor_point[0]).toString() + " " + (-sub_anchor_point[1]).toString() + ")";
+        t_str_s += "\">";
+
+        var t_str_e = "</g>";
+
+        if (aidx == 0) {
+          ret_str += jsonsvg2svg_defs(sub.defs, primary_color, secondary_color);
+          //reuse_svg = jsonsvg2svg_child(sub.layers, primary_color, secondary_color);
+          reuse_svg = mystic_symbolic_random(ctx, sub, primary_color, secondary_color);
+        }
+
+        //ret_str += jsonsvg2svg_defs(sub.defs);
+        ret_str += t_str_s;
+
+        // render svg from svgjson data
+        //
+        //ret_str += jsonsvg2svg_child(sub.layers);
+        //ret_str += mystic_symbolic_random(ctx, sub);
+        ret_str += reuse_svg;
+
+        ret_str += t_str_e;
+
+      }
+
+    }
+
+  }
+
   // nesting logic
   //
-  if ("nesting" in base.specs) {
+  ret_str += base.svg_inner;
+  if (("nesting" in base.specs) && (ctx.cur_depth <= ctx.max_nest_depth)) {
+
     var sub_idx = _irnd(ctx.data.length);
     var sub_name = ctx.data[sub_idx].name;
 
@@ -405,74 +598,23 @@ function _recur(ctx, base, sched) {
 
 
       //console.log(sub.defs);
-      ret_str += jsonsvg2svg_defs(sub.defs);
+      //ret_str += jsonsvg2svg_defs(sub.defs, primary_color, secondary_color);
+      ret_str += jsonsvg2svg_defs(sub.defs, secondary_color, primary_color );
 
       ret_str += t_str_s;
-      ret_str += jsonsvg2svg_child(sub.layers);
+      //ret_str += jsonsvg2svg_child(sub.layers, primary_color, secondary_color);
+      //ret_str += jsonsvg2svg_child(sub.layers, secondary_color, primary_color);
+      ret_str += mystic_symbolic_random(ctx, sub, secondary_color, primary_color);
       ret_str += t_str_e;
 
     }
   }
 
 
-
-
-  // attach to logic
-  //
-  var candidate_attach_list = [];
-  for (var spec_key in base_specs) {
-    if ((spec_key === "anchor") || (spec_key === "nesting")) { continue; }
-    candidate_attach_list.push(spec_key);
-  }
-  var attach_list = _choose(candidate_attach_list, complexity);
-
-  for (var attach_list_idx=0; attach_list_idx < attach_list.length; attach_list_idx++) {
-    var sub_idx = _irnd(ctx.data.length);
-    var sub_name = ctx.data[sub_idx].name;
-
-    var attach_id = attach_list[attach_list_idx];
-
-    for (var aidx=0; aidx < base.specs[attach_id].length; aidx++) {
-
-      var sub = Object.assign({}, ctx.symbol[sub_name]);
-
-      var _invert = ( ((aidx%2)==0) ? false : true );
-      var f = (_invert ? -1.0 : 1.0);
-
-      var base_attach_point = [ base.specs[attach_id][aidx].point.x, base.specs[attach_id][aidx].point.y ];
-      var base_attach_deg = _deg( base.specs[attach_id][aidx].normal.x, base.specs[attach_id][aidx].normal.y );
-
-      var sub_anchor_point = [ sub.specs.anchor[0].point.x, sub.specs.anchor[0].point.y ];
-      var sub_anchor_deg = _deg( sub.specs.anchor[0].normal.x, f*sub.specs.anchor[0].normal.y );
-
-      var deg = base_attach_deg - sub_anchor_deg;
-      if (_invert) { deg *= -1; }
-
-      var t_str_s = "<g transform=\"";
-      t_str_s += " translate(" + base_attach_point[0].toString() + " " + base_attach_point[1].toString() + ")";
-      t_str_s += " scale(" + scale.toString() + " " + (f*scale).toString() + ")";
-      t_str_s += " rotate(" + (deg).toString() + ")";
-      t_str_s += " translate(" + (-sub_anchor_point[0]).toString() + " " + (-sub_anchor_point[1]).toString() + ")";
-      t_str_s += "\">";
-
-      var t_str_e = "</g>";
-
-      ret_str += jsonsvg2svg_defs(sub.defs);
-      ret_str += t_str_s;
-
-      // render svg from svgjson data
-      //
-      ret_str += jsonsvg2svg_child(sub.layers);
-
-      ret_str += _recur(ctx, sub);
-      ret_str += t_str_e;
-
-    }
-
-  }
 
   if (top_level) {
-    ret_str += base.svg_inner;
+    ret_str += "</g>\n";
+
     ret_str += base.svg_footer;
   }
 
@@ -480,8 +622,42 @@ function _recur(ctx, base, sched) {
   return ret_str;
 }
 
-var base_symbol = g_data.symbol["hands_giving"];
+var prim_hue = Math.random();
+var prim_sat = (Math.random()*0.3) + 0.6;
+//var prim_val = Math.random();
+var prim_val = (Math.random()*0.6) + 0.3;
 
-console.log( _recur(g_data, base_symbol) );
+var seco_hue = prim_hue + 0.5;
+var seco_sat = (Math.random()*0.3) + 0.6;
+//var seco_val = Math.random();
+var seco_val = (Math.random()*0.6) + 0.3;
+if (seco_hue > 1.0) { seco_hue -= 1.0; }
+
+var prim_rgb = HSVtoRGB(prim_hue, prim_sat, prim_val);
+var seco_rgb = HSVtoRGB(seco_hue, seco_sat, seco_val);
+
+var primary_color = _rgb2hex(prim_rgb.r, prim_rgb.g, prim_rgb.b);
+var secondary_color = _rgb2hex(seco_rgb.r, seco_rgb.g, seco_rgb.b);
+
+//primary_color = undefined;
+//secondary_color = undefined;
+
+//var sched  = { "symbol": "hands_giving", "attach": { "leg": 
+
+var g_data = _preprocess_svgjson(adata, primary_color, secondary_color);
+g_data["cur_depth"] = 0;
+g_data["max_depth"] = 1;
+g_data["max_nest_depth"] = 2;
+g_data["scale"] = 0.5;
+g_data["complexity"] = 4;
+
+g_data["svg_width"] = 720.0;
+g_data["svg_height"] = 720.0;
+
+//var base_symbol = g_data.symbol["hands_claddagh"];
+var base_symbol = g_data.symbol["angel"];
+base_symbol = undefined;
+
+console.log( mystic_symbolic_random(g_data, base_symbol, primary_color, secondary_color) );
 
 //example0_a();
