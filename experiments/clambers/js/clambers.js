9ed81eb10cd4675fc35f801c46abe55e9773f4b0
abetusk <abetusk@gmail.com>
2023-06-04 10:48:22 -0400

    ...

--- experiments/clambers/js/clambers.js
+++ experiments/clambers/js/clambers.js
@@ -1,3 +1,5 @@
+// License: CC0
+//
 
 var g_ctx = {
   "data": {},
@@ -24,17 +26,66 @@ function req_json(url, cb) {
   req.send(null);
 }
 
-function clambers_init() {
-  let ele = document.getElementById("ui_button_unscramble");
-  ele.onclick = clambers_ui_unscramble;
+function ui_update_answer_list( word_list ) {
+  let ui_title = document.getElementById("ui_answer_title");
+  let ui_list = document.getElementById("ui_answer_list");
+
+  ui_list.innerHTML = '';
+
+  if (word_list.length==0) {
+    ui_title.innerHTML = 'No answer found';
+    return;
+  }
+
+  ui_title.innerHTML = 'Word list:';
+
+  for (let idx=0; idx<word_list.length; idx++) {
+    let w = word_list[idx];
+    let _li = document.createElement("li");
+    _li.appendChild(document.createTextNode(w));
+    ui_list.appendChild(_li);
+  }
+
 }
 
 function clambers_ui_unscramble() {
+
+  console.log("bang");
+
+  if (!g_ctx.ready) {
+    setTimeout(clambers_ui_unscramble, 50);
+    return;
+  }
+
   let ele = document.getElementById("ui_word");
 
   let v = ele.value;
+  let sv = letsort(v);
 
-  lw 
+  console.log(">>>", ele.value, sv, g_ctx.data[sv]);
 
-  console.log(">>>", ele.value);
+  if (sv in g_ctx.data) {
+    ui_update_answer_list( g_ctx.data[sv] );
+  }
+  else {
+    ui_update_answer_list( [] );
+  }
+
 }
+
+function ui_word_cr(x) {
+  if (typeof x === "undefined") { return; }
+  if (x.key == 'Enter') {
+    clambers_ui_unscramble();
+  }
+}
+
+function clambers_init() {
+  let ele = document.getElementById("ui_button_unscramble");
+  ele.onclick = clambers_ui_unscramble;
+  req_json("data/scram_word.json");
+
+  document.getElementById("ui_word").onkeydown = ui_word_cr;
+}
+
+
