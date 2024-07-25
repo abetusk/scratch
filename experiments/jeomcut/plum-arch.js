eb433a94ede1784085be878d716f4d883706e165
abetusk <abetusk@mechaelephant.com>
2024-07-24 20:53:44 -0400

    fixing up plum

--- experiments/jeomcut/plum-arch.js
+++ experiments/jeomcut/plum-arch.js
@@ -1,8 +1,116 @@
+// LICENSE: CC0
+//
 
-//WIP!!!
+// Y is 'up'
+// XZ plan is flat, Z comes into camera, X goes to the right
+//
+// Everything is centered aroudn 0 with unit cube cell (+-0.5, +-0.5, +-0.5)
+//
+
+var fs = require("fs");
 
 var jscad = require("@jscad/modeling");
 var stlser = require("@jscad/stl-serializer");
+var objectDeserializer = require('@jscad/obj-deserializer')
+var stlSerializer = require('@jscad/stl-serializer')
+var array_utils = require('@jscad/array-utils')
+var m4 = require("./m4.js");
+var jeom = require("./jeom.js");
+
+
+
+var op = {
+
+  "objload": objectDeserializer.deserialize,
+
+  "stldumps": stlSerializer.serialize,
+
+  "flatten": array_utils.flatten,
+
+  "clone": jscad.geometries.geom3.clone,
+  "points": jscad.geometries.geom3.toPoints,
+  "polygons": jscad.geometries.geom3.toPolygons,
+  "validate": jscad.geometries.geom3.validate,
+  "create": jscad.geometries.geom3.create,
+
+  "add" : jscad.booleans.union,
+  "sub" : jscad.booleans.subtract,
+  "or" : jscad.booleans.union,
+  "and" : jscad.booleans.intersect,
+  "scission": jscad.booleans.scission,
+
+  "expand": jscad.expansions.expand,
+  "offset": jscad.expansions.offset,
+
+  "cube": jscad.primitives.cube,
+  "cuboid": jscad.primitives.cuboid,
+  "cub": jscad.primitives.cuboid,
+  "cylinder": jscad.primitives.cylinder,
+  "sphere": jscad.primitives.sphere,
+
+  "ruboid": jscad.primitives.roundedCuboid,
+
+  "rot": jscad.transforms.rotate,
+  "rotX": jscad.transforms.rotateX,
+  "rotY": jscad.transforms.rotateY,
+  "rotZ": jscad.transforms.rotateZ,
+
+  "mul": jscad.transforms.transform,
+
+  "mov": jscad.transforms.translate,
+  "movX": jscad.transforms.translateX,
+  "movY": jscad.transforms.translateY,
+  "movZ": jscad.transforms.translateZ,
+
+  "flip": jscad.transforms.mirror,
+  "flipX": jscad.transforms.mirrorX,
+  "flipY": jscad.transforms.mirrorY,
+  "flipZ": jscad.transforms.mirrorZ,
+
+  "scale": jscad.transforms.scale,
+  "scaleX": jscad.transforms.scaleX,
+  "scaleY": jscad.transforms.scaleY,
+  "scaleZ": jscad.transforms.scaleZ,
+
+  "com" : jscad.measurements.measureCenterOfMass,
+  "bbox" : jscad.measurements.measureBoundingBox,
+  "area" : jscad.measurements.measureArea,
+  "vol" : jscad.measurements.measureVolume
+
+};
+
+function debug_geom_f(size, center, opt) {
+
+  opt = ((typeof opt === "undefined") ? [(1/32), (1/32), (1/32)] : opt);
+
+  let _s = ((typeof size === "undefined") ? [1,1,1] : size);
+  let _c = ((typeof center === "undefined") ? [0,0,0] : center);
+  let _s2 = [ _s[0]/2, _s[1]/2, _s[2]/2 ];
+
+  let _p = [
+    [ _c[0]-_s2[0], _c[0]+_s2[0] ],
+    [ _c[1]-_s2[1], _c[1]+_s2[1] ],
+    [ _c[2]-_s2[2], _c[2]+_s2[2] ]
+  ];
+
+  let _debug_geom =
+    op.add(
+      op.mov([ _p[0][0], _p[1][0], _p[2][0] ], op.cub({"size":opt})),
+      op.mov([ _p[0][1], _p[1][0], _p[2][0] ], op.cub({"size":opt})),
+      op.mov([ _p[0][0], _p[1][1], _p[2][0] ], op.cub({"size":opt})),
+      op.mov([ _p[0][0], _p[1][0], _p[2][1] ], op.cub({"size":opt})),
+
+      op.mov([ _p[0][1], _p[1][1], _p[2][0] ], op.cub({"size":opt})),
+      op.mov([ _p[0][0], _p[1][1], _p[2][1] ], op.cub({"size":opt})),
+      op.mov([ _p[0][1], _p[1][0], _p[2][1] ], op.cub({"size":opt})),
+
+      op.mov([ _p[0][1], _p[1][1], _p[2][1] ], op.cub({"size":opt}))
+    );
+
+  return _debug_geom;
+}
+//var DEBUG_GEOM = debug_geom_f([1,1,1], [0.5,0.5,0.5]);
+var DEBUG_GEOM = debug_geom_f([1,1,1], [0,0,0]);
 
 
 function pnt_eq( p0, p1, _eps) {
@@ -93,27 +201,10 @@ function _main() {
   make_arch(2);
 }
 
-function jscad_f() {
-  let f = {
-    "sub":jscad.booleans.subtract,
-    "and":jscad.booleans.intersect,
-    "add":jscad.booleans.union,
-    "mov":jscad.transforms.translate,
-    "rot":jscad.transforms.rotate,
-    "lif":jscad.extrusions.extrudeLinear,
-    "cub":jscad.primitives.cuboid,
-    "cir":jscad.primitives.circle
-  }
-
-  return f;
-}
-
-function wedge_u() {
-  let f = jscad_f();
-
-  let geom = f.sub(
-    f.cub({"size":[1,1,1], "center":[0.5, 0.5, 0.5]}),
-    f.rot([Math.PI/4,0,0], f.cub({"size":[2,2,2], "center":[0,1,-1]}))
+function wedge_up() {
+  let geom = op.sub(
+    op.cub({"size":[1,1,1], "center":[0, 0, 0]}),
+    op.rot([Math.PI/4,0,0], op.cub({"size":[2,2,2], "center":[0,0.0,-1.0]}))
   );
 
   return [
@@ -121,12 +212,10 @@ function wedge_u() {
   ];
 }
 
-function wedge_d() {
-  let f = jscad_f();
-
-  let geom = f.sub(
-    f.cub({"size":[1,1,1], "center":[0.5, 0.5, 0.5]}),
-    f.rot([Math.PI/4,0,0], f.cub({"size":[2,2,2], "center":[0,1,1]}))
+function wedge_down() {
+  let geom = op.sub(
+    op.cub({"size":[1,1,1], "center":[0, 0, 0]}),
+    op.rot([Math.PI/4,0,0], op.cub({"size":[2,2,2], "center":[0,0,1]}))
   );
 
   return [
@@ -134,42 +223,40 @@ function wedge_d() {
   ];
 }
 
-function stair(n) {
+// y up
+// stairs facing -z direction, sides in +-x
+// slabs in xz plane, going up y
+//
+// bottom step needs to start in a step and top step
+// needs to be missing as the diagonal element will
+// be a full block, taking the place of the steps.
+//
+function stair(n, _debug) {
   n = ((typeof n === "undefined") ? 3 : n);
-  let f = jscad_f();
-
-
-  /*
-  let geom = f.add(
-    f.cub({"size":[1,3/4,1/4], "center":[1/2, 1/2+1/8, 1/2-1/4-1/8]}),
-    f.cub({"size":[1,2/4,1/4], "center":[1/2, 1/2+2/8, 1/2-0/4-1/8]}),
-    f.cub({"size":[1,1/4,1/4], "center":[1/2, 1/2+3/8, 1/2+1/4-1/8]})
-  );
-  */
 
   let geom = {};
 
   let N = n+1;
   for (let i=0; i<n; i++) {
-    let yi = i - ((n-1)/2);
+    let zi = i - ((n-1)/2);
 
     let sx = 1,
-        sy = (n-i)/N,
-        sz = 1/N;
+        sy = 1/N,
+        sz = (n-i)/N;
 
-    let cx = 1/2,
-        cy = (1/2) + ((i+1)/(2*N)),
-        cz = (1/2) - (1/(2*N)) + (yi/N);
-    let s = f.cub({"size":[sx,sy,sz], "center":[cx,cy,cz]});
+    let cx = 0,
+        cy = (zi/N) - (1/(2*N)),
+        cz = ((i+1)/(2*N));
+    let s = op.cub({"size":[sx,sy,sz], "center":[cx,cy,cz]});
 
 
-    if (i==0) {
-      geom = s;
-    }
-    else {
-      geom = f.add(geom, s);
-    }
+    if (i==0) { geom = s; }
+    else      { geom = op.add(geom, s); }
 
+  }
+
+  if (_debug) {
+    geom = op.add(geom, DEBUG_GEOM);
   }
 
   return [
@@ -177,17 +264,18 @@ function stair(n) {
   ];
 }
 
-//WIP!!!
 function block() {
-  let f = jscad_f();
-  let geom = f.mov([0.5,0.5,0.5], f.cub({"size":[1,1,1]}));
+
+  let geom = op.mov([0,0,0], op.cub({"size":[1,1,1]}));
   return [
     {"ds":[0,0,0], "geom":geom, "id":"b", "nei":["b","b", ".",".", "b,.","b"]}
   ];
 }
 
 function arch1() {
-  let f = jscad_f();
+  //let f = jscad_f();
+  let f = op;
+
   /*
   let sub = jscad.booleans.subtract;
   let and = jscad.booleans.intersection;
@@ -217,7 +305,8 @@ function arch1() {
 }
 
 function arch2() {
-  let f = jscad_f();
+  //let f = jscad_f();
+  let f = op;
 
   /*
   let sub = jscad.booleans.subtract;
@@ -252,8 +341,8 @@ function arch2() {
 }
 
 function arch3() {
-
-  let f = jscad_f();
+  //let f = jscad_f();
+  let f = op;
 
   /*
   let sub = jscad.booleans.subtract;
@@ -311,8 +400,8 @@ function arch3() {
 }
 
 function construct_voxel_dock(info, rot_info) {
-
-  let f = jscad_f();
+  //let f = jscad_f();
+  let f = op;
 
   let irot = 1;
 
@@ -340,6 +429,34 @@ function construct_voxel_dock(info, rot_info) {
 
 function main() {
 
+  let _lib_info = [
+    { "name": "stair", "f": stair },
+    { "name": "block", "f": block },
+    { "name": "wedge_up", "f": wedge_up },
+    { "name": "wedge_down", "f": wedge_down },
+    { "name": "arch", "f": null }
+  ];
+
+  let lib_info = [
+    { "name": "block",        "f": function() { return block(); } },
+    { "name": "wedge_up",     "f": function() { return wedge_up(); } },
+    { "name": "wedge_down",   "f": function() { return wedge_down(); } },
+    { "name": "stair",        "f": (function(_n){ return function() { return stair(_n); } })(5)  }
+  ];
+
+
+  for (let li_idx=0; li_idx<lib_info.length; li_idx++) {
+    let li = lib_info[li_idx];
+
+    console.log(">>", li.name);
+
+    let shape_info = li.f();
+    let stl_data = stlser.serialize({"binary":false}, shape_info[0].geom).join("");
+    fs.writeFileSync(".plum_stl/" + li.name + ".stl", stl_data);
+  }
+
+  return;
+
   /*
   let sub = jscad.booleans.subtract;
   let and = jscad.booleans.intersection;
@@ -354,7 +471,8 @@ function main() {
 
   let ds = 1/32;
 
-  let f = jscad_f();
+  //let f = jscad_f();
+  let f = op;
 
   let refcubes =
     f.add(
